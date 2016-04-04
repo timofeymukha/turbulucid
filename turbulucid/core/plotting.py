81b06dd38011873391666091c6151eda1fffd5e8
Timofey Mukha <timofey.mukha@it.uu.se>
2016-04-04 13:39:53 +0200

    plotting.py: pep8 adjustmens.

--- turbulucid/core/plotting.py
+++ turbulucid/core/plotting.py
@@ -10,5 +10,5 @@ def plot_field(field, cmap="RdBu_r", aspect="equal", **kwargs):
     pcm = plt.pcolormesh(field["X"], field["Y"], field["V"], cmap='RdBu_r',
                          **kwargs)
     pcm.axes.set_aspect(aspect)
-    plt.xlim([np.min(field["X"][:,0]), np.max(field["X"][:, -1])])
-    plt.ylim([np.min(field["Y"][0,:]), np.max(field["Y"][-1, :])])
+    plt.xlim([np.min(field["X"][:, 0]), np.max(field["X"][:, -1])])
+    plt.ylim([np.min(field["Y"][0, :]), np.max(field["Y"][-1, :])])
