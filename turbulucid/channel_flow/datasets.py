4ff23d2420d3041768d50fb223092803fdd8203f
Timofey Mukha <timofey.mukha@it.uu.se>
2016-06-08 16:37:02 +0200

    Enchancing channel flow dataset.

--- turbulucid/channel_flow/datasets.py
+++ turbulucid/channel_flow/datasets.py
@@ -3,6 +3,7 @@ import sys
 
 __all__ = ["dns_lee_moser"]
 
+
 def dns_lee_moser():
     """
     Returns the path to the DNS dataset by Lee and Moser for channel flow.
@@ -10,3 +11,19 @@ def dns_lee_moser():
 
     return os.path.join(sys.modules[__name__].__file__, "..","datasets",
                         "channel_dns_lee_moser.hdf5")
+
+
+def dns_lee_moser_retau():
+    return [182.088, 543.396, 1000.512, 1994.756, 5185.879]
+
+
+def dns_lee_moser_retheta():
+    return [290.308, 1005.680, 1930.604, 3968.745, 10182.024]
+
+
+def dns_lee_moser_redelta99():
+    return [2797.662, 9567.261, 18895.102, 40169.6495, 111714.806]
+
+
+def dns_lee_moser_redeltastar():
+    return [469.305, 1413.505, 2604.424, 5185.416, 12816.696]
