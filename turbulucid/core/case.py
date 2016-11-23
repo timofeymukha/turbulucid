0f63966c5570d8b437b9645776efb2a919bbd28b
Timofey Mukha <timofey.mukha@it.uu.se>
2016-11-23 15:55:28 +0100

    Added function for extracting boundary data

--- turbulucid/core/case.py
+++ turbulucid/core/case.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import numpy as np
+from vtk.numpy_interface import dataset_adapter as dsa
+from vtk.util.numpy_support import numpy_to_vtk
 import os
 import vtk
 from vtk.numpy_interface import dataset_adapter as dsa
@@ -65,4 +67,50 @@ class Case:
 
         """
         return self.vtkData.CellData[item]
+
+    def extract_boundary_cells(self, boundary):
+        """Extract cells adjacent to a certain boundary.
+
+        Parameters
+        ----------
+            boundary : str
+                The name of the boundary.
+
+        Returns
+        -------
+            vtkExtractSelection
+
+        """
+        if boundary not in self.vtkData.FieldData.keys():
+            raise(NameError("No boundary named "+boundary))
+
+        cellIds = self.vtkData.FieldData[boundary]
+        selectionNode = vtk.vtkSelectionNode()
+        selectionNode.SetFieldType(vtk.vtkSelectionNode.CELL)
+        selectionNode.SetContentType(vtk.vtkSelectionNode.INDICES)
+        selectionNode.SetSelectionList(numpy_to_vtk(cellIds))
+
+        selection = vtk.vtkSelection()
+        selection.AddNode(selectionNode)
+
+        extractSelection = vtk.vtkExtractSelection()
+        extractSelection.SetInputConnection(0, self.reader.GetOutputPort())
+
+        extractSelection.SetInputData(1, selection)
+        extractSelection.Update()
+
+        return extractSelection
+
+    def boundary_cell_data(self, boundary):
+
+        selection = self.extract_boundary_cells(boundary)
+        cCenters = vtk.vtkCellCenters()
+        cCenters.SetInputData(selection.GetOutput())
+        cCenters.Update()
+
+        coords = dsa.WrapDataObject(cCenters.GetOutput()).Points
+        data = dsa.WrapDataObject(selection.GetOutput()).CellData
+
+        return coords, data
+
 
