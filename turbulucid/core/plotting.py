fa1e87b27d31ae8abcc7d24a581bc503d80c0d3d
Timofey Mukha <timofey.mukha@it.uu.se>
2016-11-23 15:54:37 +0100

    Moved  2d plotting to legacy module

--- turbulucid/core/plotting.py
+++ turbulucid/core/plotting.py
@@ -1,17 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import numpy as np
-import matplotlib.pyplot as plt
 
-__all__ = ["plot_field"]
+__all__ = []
 
 
-def plot_field(field, cmap="RdBu_r", aspect="equal", **kwargs):
-    """ Plot the 2d field as a contour plot."""
-
-    pcm = plt.pcolormesh(field["X"], field["Y"], field["V"], cmap='RdBu_r',
-                         **kwargs)
-    pcm.axes.set_aspect(aspect)
-    plt.xlim([np.min(field["X"][:, 0]), np.max(field["X"][:, -1])])
-    plt.ylim([np.min(field["Y"][0, :]), np.max(field["Y"][-1, :])])
