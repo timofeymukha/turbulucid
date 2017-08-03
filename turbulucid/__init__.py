4255231610f2218d3fd4d1d131100ad8fbdbb726
Timofey Mukha <timofey.mukha@it.uu.se>
2017-08-03 13:09:35 +0200

    Added two BFS datasets

--- turbulucid/__init__.py
+++ turbulucid/__init__.py
@@ -1,7 +1,8 @@
 from . import core
 from .flows import zpgtbl
 from .flows import channel_flow
+from .flows import bfs
 from .core import *
 
-__all__ = ["core", "zpgtbl", "channel_flow"]
+__all__ = ["core", "zpgtbl", "channel_flow", "bfs"]
 __all__.extend(core.__all__)
--- turbulucid/flows/__init__.py
+++ turbulucid/flows/__init__.py
@@ -1,2 +1,3 @@
 from . import channel_flow
 from . import zpgtbl
+from . import bfs
