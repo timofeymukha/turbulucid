9d4a84b8f7265578466a3da5e27c3399f7bb9ed5
Timofey Mukha <timofey.mukha@it.uu.se>
2018-08-06 16:47:30 +0200

    Added licence and copyright headers to files with tests. Working on  .travis.yml

--- tests/core/test_readers.py
+++ tests/core/test_readers.py
@@ -1,3 +1,8 @@
+# This file is part of turbulucid
+# (c) 2018 Timofey Mukha
+# The code is released under the GNU GPL Version 3 licence.
+# See LICENCE.txt and the Legal section in the README for more information
+
 from __future__ import print_function
 from __future__ import division
 import vtk
