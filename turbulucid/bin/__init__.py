c11940d9b9c7c0cb88990e544e3e1ab7f5d91f31
Timofey Mukha <timofey.mukha@it.uu.se>
2018-08-06 16:37:49 +0200

    Upgraded headers to include copyright year. Working on  .travis.yml

--- turbulucid/bin/__init__.py
+++ turbulucid/bin/__init__.py
@@ -1,4 +1,4 @@
 # This file is part of turbulucid
-# (c) Timofey Mukha
+# (c) 2018 Timofey Mukha
 # The code is released under the GNU GPL Version 3 licence.
 # See LICENCE.txt and the Legal section in the README for more information
--- turbulucid/core/data_extraction.py
+++ turbulucid/core/data_extraction.py
@@ -1,5 +1,5 @@
 # This file is part of turbulucid
-# (c) Timofey Mukha
+# (c) 2018 Timofey Mukha
 # The code is released under the GNU GPL Version 3 licence.
 # See LICENCE.txt and the Legal section in the README for more information
 
